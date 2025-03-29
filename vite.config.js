fe27897f8e8c6fc9bb54137923cde9d66da7b428
techking11 <nma1132000@gmail.com>
2025-03-29 12:34:50 +0700

    initial: enviroment setup

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+    coverage: {
+      provider: 'v8',
+    },
+  },
+});
